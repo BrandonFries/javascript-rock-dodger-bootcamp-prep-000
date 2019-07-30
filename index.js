d0154a6cc602ffeea59a673aeb5ef69a4eac43d2
Brandon Fries <brandonfries1993@gmail.com>
2019-07-30 23:30:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function checkCollision(rock){
 }
 
 function createRock(x) {
-  const rock = document.createElement('div');
+  var rock = document.createElement('div');
   rock.className = 'rock';
   rock.style.left = `${x}px`;
   var top = rock.style.top = 0
